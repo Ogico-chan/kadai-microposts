9978fc5cbb816977a85f3f6e6932908de0fd2f4a
kazuya.ogiwara <akaisuisei242000@yahoo.co.jp>
2020-11-14 07:10:58 +0000

    kadai

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,14 @@ Rails.application.routes.draw do
       get :followers
     end
   end
+  
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :likes
+    end
+  end
 
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :likes, only: [:create, :destroy]
 end
